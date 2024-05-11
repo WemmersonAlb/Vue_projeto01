c361bc8fcda00762f290b0844bc4efae823a57c2
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2024-05-11 10:08:45 -0300

    [feat] Adição de formulário Está adição serve para auxiliar no entendimento do funcionamento de eventos como @submit. Estes eventos são fundamentais para o funcionamento do VUE Js.
    Ao preencher o formulário, o botão de enviar reflete o preenchimento do usuário no console do navegador.

--- VuePorCLI/aprendendo_vue_01/src/App.vue
+++ VuePorCLI/aprendendo_vue_01/src/App.vue
@@ -3,6 +3,7 @@ import LifeCycle from './components/LifeCycle.vue'
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import Pessoa from './components/Pessoa.vue'
 import Picture from './components/Picture.vue'
+import Formulario from './components/Formulario.vue'
 
 export default {
   name: 'App',
@@ -10,26 +11,31 @@ export default {
     PrimeiroComponente,
     LifeCycle,
     Pessoa,
-    Picture
+    Picture,
+    Formulario
   }
 }
 </script>
 
 <template>
-  <div>
+  <div class="div-mae">
     <div class="foto-texto">
       <Picture class="picture"/>
       <PrimeiroComponente />
     </div>
     <Pessoa />
+    <Formulario />
   </div>
 </template>
-<style>
+<style scoped>
   .foto-texto{
     display: flex;
     flex-direction: row;
   }
   .picture{
     margin-right: 10px;
+  }
+  .div-mae{
+    padding:100px 0;
   }
 </style>
---
+++ VuePorCLI/aprendendo_vue_01/src/components/Formulario.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: 'Formulario',
+    data(){
+        return {
+            nome:'',
+            mensagem: ''
+        }
+    },
+    methods:{
+        enviarFormulario(e){
+            e.preventDefault();            
+            console.log('Formulário enviado com sucesso');
+            console.log('Nome: '+this.nome);
+            console.log('Mensagem: '+this.mensagem);
+        }
+    }
+}
+</script>
+<template>
+    <div>
+        <h2>Deixe uma mensagem</h2>
+        <form action="" @submit="enviarFormulario">
+            <input type="text" placeholder="Nome" v-model="nome">
+            <textarea cols="30" rows="10" placeholder="Mensagem" v-model="mensagem"></textarea>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</template>
+<style>
+    form{
+        display: flex;
+        flex-direction: column;
+    }
+    input, textarea, button{
+        background-color: rgb(58, 58, 58);
+        margin-top:10px;
+        resize: none;
+    }
+    button{
+        border: 1px solid #00bd7e;
+        color:#00bd7e;
+        padding: 5px 0;
+    }
+    input, textarea{
+        padding: 5px;
+        color: rgb(148, 149, 150);
+    }
+</style>
